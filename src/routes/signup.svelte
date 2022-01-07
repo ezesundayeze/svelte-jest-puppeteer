dabbcc309b07edc6af31558fe85f78c5e2293bf3
Eze Sunday Eze <mailsforeze@gmail.com>
2022-01-07 15:09:09 +0100

    add app test

--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -1,38 +1,46 @@
 <script>
 	import { useForm, validators, HintGroup, Hint, email, required } from 'svelte-use-form';
-    import "../signup.css";
+	import '../signup.css';
 
 	const form = useForm();
 </script>
 
 <form use:form>
 	<h1>Signup</h1>
-    <label for="Email" class="label-email">
-        <input type="email" use:validators={[required, email]} class="text" id="email" name="email" placeholder="Email" required />
-      <span class="required">Email</span>
-    </label>
-	<HintGroup  for="email">
+	<label for="Email" class="label-email">
+		<input
+			type="email"
+			use:validators={[required, email]}
+			class="text"
+			id="email"
+			name="email"
+			placeholder="Email"
+			required
+		/>
+		<span class="required">Email</span>
+	</label>
+	<HintGroup for="email">
 		<Hint on="required" class="email-required-hint">Email is required</Hint>
-		<Hint on="email" class="email-hint"  hideWhenRequired>Email is not valid</Hint>
+		<Hint on="email" class="email-hint" hideWhenRequired>Email is not valid</Hint>
 	</HintGroup>
-    
-      <div>
-        <label class="label-password">
-          <input type="password" class="text" use:validators={[required]}  name="password" placeholder="Password" id="password" required />
-          <span class="required">Password</span>
-        </label>
-      </div>
-	<Hint for="password" on="required" class="password-required-hint">Password is required</Hint>
-    <div>
-        <label class="label-password">
-          <input type="text" class="text" name="password" placeholder="Password" id="password2" required />
-          <span class="required">Password</span>
-        </label>
-      </div>
-      <Hint for="password" on="required">This is a required field</Hint>
 
-      <input type="submit" disabled={!$form.valid} id="submit" value="Signup" />
+	<div>
+		<label class="label-password">
+			<input
+				type="password"
+				class="password"
+				use:validators={[required]}
+				name="password"
+				placeholder="Password"
+				id="password"
+				required
+			/>
+			<span class="required">Password</span>
+		</label>
+	</div>
+	<Hint for="password" name="password-required-hint" on="required" id="password-required-hint" class="password-required-hint">Password is required</Hint>
 
+	<input type="submit" disabled={!$form.valid} id="submit" value="Signup" />
 </form>
 
 <style>
--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,3 @@
-
-const timeout = process.env.SLOWMO ? 30000 : 40000;
-
 beforeAll(async () => {
     await page.goto(URL, {waitUntil: 'domcontentloaded'});
 });
@@ -9,7 +6,7 @@ describe('Header Testing', () => {
     test('Page title', async () => {
         const title = await page.title();
         expect(title).toBe('Home');
-    }, timeout);
+    });
 });
 
 describe('Signup form', () => {
@@ -20,21 +17,17 @@ describe('Signup form', () => {
         await page.click('body');
         const emailHint = await page.$eval('.email-hint', el => el.innerHTML);
         expect(emailHint).toBe('Email is not valid');
-    }, timeout);
+    });
 
-
-    test('Test empty fields', async () => {   
+    test('Test empty field hints', async () => {
         await page.goto(URL + '/signup', {waitUntil: 'domcontentloaded'});
         await page.waitForSelector('form');
         await page.type('#email', '');
-        await page.type('#password', '');
         await page.click('body');
-        const passwordRequired = await page.$eval('.password-required-hint', el =>el.innerHTML);
-        const emailRequired = await page.$eval('.email-required-hint', el =>el.innerHTML);
 
-        expect(passwordRequired).toBe('Password is required');
-        expect(emailRequired).toBe('Email is required');
-    }, timeout);
+        const emailHint = await page.$eval('.email-required-hint', el => el.innerHTML);
+        expect(emailHint).toBe('Email is required');
+    });
 });
 
 
